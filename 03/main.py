945a63b9117d8b30e6378c3caa28e1956d247347
Cristhian <cristhianclx@gmail.com>
2025-12-02 22:27:17 -0500

    03

--- 03/main.py
+++ 03/main.py
@@ -8,22 +8,40 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, render_as_batch=True)
 
 
 class Student(db.Model):
     
     __tablename__ = "students"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(200))
     last_name = db.Column(db.String(200))
     age = db.Column(db.Integer)
     country = db.Column(db.String(10))
+    city = db.Column(db.String(50))
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
     def __repr__(self):
         return "<Student: {}>".format(self.id) # <Student: 1>
+
+
+class Message(db.Model):
+
+    __tablename__ = "messages"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(50))
+    content = db.Column(db.Text)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+    student_id = db.Column(db.Integer, db.ForeignKey("students.id"))
+    student = db.relationship("Student", backref="student")
+
+    def __repr__(self):
+        return "<Message: {}>".format(self.id) # <Message: 1>
+
 
 
 @app.route("/status/")
@@ -31,6 +49,3 @@ def status():
     return {
         "status": "live"
     }
-
-
-# en el modelo Student, agregar una columna city, String(50)
